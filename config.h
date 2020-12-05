48837e95973c0e34504d6bbce4dd6e0e2267b180
Dhaval Patel <dhaval.create@gmail.com>
2020-12-05 15:21:01 +0530

    added patch 'dmenu-fuzzyhighlight'

--- config.h
+++ config.h
@@ -12,6 +12,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#BA8866", "#282828" },
 	[SchemeSel] = { "#282828", "#BA8866" },
+	[SchemeSelHighlight] = { "#BA8866", "#282828" },
+	[SchemeNormHighlight] = { "#282828", "#BA8866" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
